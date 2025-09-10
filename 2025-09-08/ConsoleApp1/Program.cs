e3b5cea02ec5c70ecf38e227a1ad260c496cbebd
nbestfator@gmail.com <nbestfator@gmail.com>
2025-09-10 19:03:18 +0300

    Добавлен проект Робот

---
+++ 2025-09-08/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Robot robotAllan = new Robot("Аллан", "модель 550", 123456789, "Строительный");
+
+System.Console.WriteLine(robotAllan.Name);
+
+robotAllan.Model = "модель 111";
+System.Console.WriteLine(robotAllan.Model);
+
+robotAllan.ShowInfo();
+
+Robot robotArnold = new Robot("Арнольд", "T-800", 101, "Инфильтратор");
+
+public class Robot
+{
+    public string Name;
+    public string Model;
+    public int SerialNumber;
+    public string Type;
+
+    public Robot(string name, string model, int serialNumber, string type)
+    {
+        this.Name = name;
+        this.Model = model;
+        this.SerialNumber = serialNumber;
+        this.Type = type;
+    }
+
+
+    public void ShowInfo()
+    {
+        System.Console.WriteLine($"Я робот\n" +
+        $"\t Моё имя {this.Name}\n" +
+        $"\t Моя модель {this.Model}\n" +
+        $"\t Мой серийный номер {this.SerialNumber}\n" +
+        $"\t Моё назначение {this.Type}\n");
+    }
+}
---
+++ 2025-09-09/09.09.25/Program.cs
@@ -0,0 +1,76 @@
+﻿
+
+
+
+Robot robotTom = new Robot("Tom", "Повар", 1050);
+
+
+Console.WriteLine(robotTom.SerialNumber);
+
+Console.WriteLine(robotTom.RobotName);
+
+robotTom.SetName("Bob");
+
+Console.WriteLine(robotTom.RobotName);
+
+
+public class Robot
+
+{
+    //вариант 1
+    private string _robotName;
+
+
+    public string RobotName => _robotName;
+
+
+    public void SetName(string name)
+
+
+    {
+        _robotName = name;
+    }
+
+
+
+    //вариант 2
+
+
+    
+
+    //вариант 3
+
+    private string _model;
+
+    public string Model 
+    {
+        get { return Model; }
+        set
+        {
+
+
+            if (Model == "spy")
+            {
+                string[] models = { "1200", "1300", "1400" };
+
+            }
+            else
+            {
+                _model = value;
+            }
+        }
+
+    }
+
+    public Robot(string name, string model, int serialNumber)
+    {
+        _robotName = name;
+        Model = model;
+        SerialNumber = serialNumber;
+    }
+
+
+   
+}
+
+
